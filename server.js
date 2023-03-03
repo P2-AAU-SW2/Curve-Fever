eb6c962c3b99c6808986c3d20dec23245e5f2cd6
ollioddi <oliver@roedscholer.dk>
2023-03-03 22:07:50 +0100

    More testing

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 require('dotenv').config()
 const app = express()
-const PORT = process.env.PORT;
+
+//const PORT = process.env.PORT;
+const PORT = 3000;
 const HOST = process.env.HOST;
 
 app.use(express.static('public'));
