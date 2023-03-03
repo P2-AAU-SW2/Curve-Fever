855a545532a090e7de0c5b97efc73b613727be5c
ollioddi <oliver@roedscholer.dk>
2023-03-03 20:47:18 +0100

    Added dockerfile

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 require('dotenv').config()
 const app = express()
-const port = process.env.PORT;
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
 
 app.use(express.static('public'));
 
-app.listen(port, () => {
-  console.log(`app listening on port ${port}`)
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`)
 })
