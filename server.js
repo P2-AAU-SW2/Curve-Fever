be884314d19859be6c531baf6adfb1c6173d592b
ollioddi <oliver@roedscholer.dk>
2023-03-12 18:33:19 +0100

    Setup stuff

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-const express = require('express')
-require('dotenv').config()
-const app = express()
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const logger = require('morgan');
+const path = require('path');
 
 //const PORT = process.env.PORT;
 const PORT = 3000;
 const HOST = process.env.HOST;
 
-app.use(express.static('public'));
+// Setup
+app.use(logger('dev'));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+const indexRouter = require('./routes/index');
+const loginRouter = require('./routes/login');
+const homeRouter = require('./routes/home');
+
+// Router setup
+app.use('/', indexRouter)
+app.use('/login', loginRouter);
+app.use('/home', homeRouter);
+
 
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`)
