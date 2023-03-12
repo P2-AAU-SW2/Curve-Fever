5b565439578f885754a839e050cfc9a535dce193
ollioddi <oliver@roedscholer.dk>
2023-03-12 21:18:03 +0100

    Examples

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get("/", function (req, res) {
-  res.render("login/loginPage");
+  res.render("loginPage");
 });
 
 module.exports = router;
