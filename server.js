3715d1cdd0e5ae50fadb12186d0d1c21617dc144
Luca Purcilly <luca.purcilly@gmail.com>
2023-03-20 13:27:34 +0100

    Added gameArea
    Co-authored-by: aneekhussain <aneekhussain@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const logger = require('morgan');
-const path = require('path');
-require('dotenv').config();
+const logger = require("morgan");
+const path = require("path");
+require("dotenv").config();
 
 // Setup
 const PORT = process.env.PORT || 3000;
-app.use(logger('dev'));
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({ extended: true}));
+app.use(logger("dev"));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: true }));
 
-const indexRouter = require('./routes/index');
-const loginRouter = require('./routes/login');
-const homeRouter = require('./routes/home');
+const indexRouter = require("./routes/index");
+const loginRouter = require("./routes/login");
+const homeRouter = require("./routes/home");
+const gameAreaRouter = require("./routes/gameArea");
 
 // Router setup
-app.use('/', indexRouter);
-app.use('/login', loginRouter);
-app.use('/home', homeRouter);
-
+app.use("/", indexRouter);
+app.use("/login", loginRouter);
+app.use("/home", homeRouter);
+app.use("/gameArea", gameAreaRouter);
 // Testing
-app.get('/version', function(req, res) {
-  res.json({version: process.env.npm_package_version})
-})
+app.get("/version", function (req, res) {
+  res.json({ version: process.env.npm_package_version });
+});
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Running on port ${PORT}`)
-})
+  console.log(`Running on port ${PORT}`);
+});
