7f0ffe16883fa82fbc1c0bd34d47ad334d988136
Luca Purcilly <luca.purcilly@gmail.com>
2023-04-11 19:34:49 +0200

    Minor updates to gamearea

--- public/javascript/gameArea.js
+++ public/javascript/gameArea.js
@@ -1,3 +1,6 @@
-let canvas = document.getElementById("gameCanvas");
-canvas.width = Math.min(window.innerWidth, window.innerHeight);
-canvas.height = Math.min(window.innerWidth, window.innerHeight);
+const playerCells = document.querySelectorAll("#scoretable td:nth-child(2)");
+const playerColors = ["#32BEFF", "#FF6DDE", "#FFF116", "#B9FF32", "#FF6D6D"];
+
+for (let i = 0; i < playerCells.length; i++) {
+  playerCells[i].style.color = playerColors[i % playerColors.length];
+}
