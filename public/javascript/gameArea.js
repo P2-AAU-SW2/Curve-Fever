e79a303a4ac684e7a2252e65e6887205030b041b
Luca Purcilly <luca.purcilly@gmail.com>
2023-04-11 10:05:32 +0200

    routed gamearea to quickplay button

---
+++ public/javascript/gameArea.js
@@ -0,0 +1,3 @@
+let canvas = document.getElementById("gameCanvas");
+canvas.width = Math.min(window.innerWidth, window.innerHeight);
+canvas.height = Math.min(window.innerWidth, window.innerHeight);
