2043c48d223a415a81193c77c3eee198eb9bfc01
risobak <nicolasobakke@gmail.com>
2023-04-12 18:15:30 +0200

    Add responsiveness and UI improvements

--- routes/home.js
+++ routes/home.js
@@ -1,17 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const version = "0.0.1";
 
 router.get("/", function (req, res) {
-  console.log*("Here");
-  res.render("home")
+	console.log * "Here";
+	res.render("home");
 });
 
 router.get("/login", function (req, res) {
-  res.render("home_login")
-})
-
-
+	res.render("home_login");
+});
 
 module.exports = router;
