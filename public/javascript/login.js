5094474ccef23e8fad946dce9822a0a45ef45fe8
risobak <nicolasobakke@gmail.com>
2023-04-15 17:04:33 +0200

    Add form slide animation

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,18 +1,4 @@
-const forgotPasswordP = document.getElementById("forgotPassword");
-
-forgotPasswordP.addEventListener("click", (e) => {
-    document.getElementById("pwd-modal").style.display = "block";
-});
-
-const pages = document.querySelectorAll(".page");
-const translateAmount = 100;
-let translate = 0;
-
-slide = (direction) => {
-    direction === "next"
-        ? (translate -= translateAmount)
-        : (translate += translateAmount);
-    pages.forEach((pages) => {
-        pages.style.transform = `translateX(${translate}%)`;
-    });
+slide = () => {
+    const pages = document.querySelector(".pages");
+    pages.classList.toggle("slide-left");
 };
