cf62ad3cec96ab94f3ee34ea215024f183bcf27a
ollioddi <oliver@roedscholer.dk>
2023-04-21 09:56:47 +0200

    Add link to terminal

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 3000;
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}`);
+    console.log(`Running on port http://localhost:${PORT}`);
 });
