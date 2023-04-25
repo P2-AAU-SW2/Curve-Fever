d88f6b20f3c2d02e8a5d642dad94a6d09b2fca15
ollioddi <oliver@roedscholer.dk>
2023-04-25 20:48:38 +0200

    Fix host

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { httpServer } = require("./app");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || "localhost";
+const HOST = process.env.HOST || "127.0.0.1";
 
 httpServer.listen(PORT, HOST, () => {
     console.log(`Server is running: http://${HOST}:${PORT}`);
