4375fca5df2e0b6fac531d7030f768654718f1fd
risobak <nicolasobakke@gmail.com>
2023-05-20 21:24:30 +0200

    Fix #11; 23#; Add responsiveness to login and auto fill username
    Made some ui fixes and made it more responsive. I also made it so if you insert a guestname / username the value is then inserted into each others field

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,4 +1,29 @@
+function submitSignup() {
+    const form = document.getElementById("authForm");
+    form.action = "/login/createUser";
+    form.method = "post";
+    form.submit();
+}
+
+function submitLogin() {
+    const form = document.getElementById("authForm");
+    form.action = "/login/login";
+    form.method = "post";
+    form.submit();
+}
+
+function submitGuest() {
+    const form = document.getElementById("guestForm");
+    form.action = "/login/guest";
+    form.method = "post";
+    form.submit();
+}
 slide = () => {
+    const guestName = document.querySelector("#guestname");
+    const userName = document.querySelector("#username");
     const pages = document.querySelector(".pages");
+    if (!pages.classList.contains("slide-left"))
+        userName.value = guestName.value;
+    else guestName.value = userName.value;
     pages.classList.toggle("slide-left");
 };
